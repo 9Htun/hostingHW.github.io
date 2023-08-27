7bfcce8f608092cf9abb80152fb7a35f675edd43
9Htun <9.bluelake@gmail.com>
2023-08-27 12:01:26 +0630

    no message

--- patient/View/home1.php
+++ patient/View/home1.php
@@ -88,7 +88,7 @@ if (!isset($_SESSION["email"])) {
                         <p class="text-white"> <?= $info["para_text"] ?></p>
                     </div>
                     <div class="article_image">
-                        <img src="./storages/article/<?= $info["image"] ?>" alt="" id="article_image">
+                        <img src="../../admin/View/storages/article/<?= $info["image"] ?>" alt="" id="article_image">
                     </div>
                 </div>
             </div>
@@ -107,7 +107,7 @@ if (!isset($_SESSION["email"])) {
                 <li class="card">
                     <div>
                         <div class="vaccine_div">
-                            <img src="./storages/medicine/<?= $medicine["medicine_image"] ?>" class="vaccine_img">
+                            <img src="../../admin/View/storages/medicine/<?= $medicine["medicine_image"] ?>" class="vaccine_img">
                         </div>
                         <h3 class="card-title"><?= $medicine["medicine_name"] ?></h3>
                         <div class="card-content">
